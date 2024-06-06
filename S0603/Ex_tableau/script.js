0db13cb6397a6125897b94938d6bf0bb7628c6da
Melusine Chrs <melusine.chrs@gmail.com>
2024-06-07 00:00:34 +0200

    exo tableau 06.06

--- S0603/Ex_tableau/script.js
+++ S0603/Ex_tableau/script.js
@@ -1,22 +1,22 @@
 ///////   EXEMPLE   ///////
 
-const yellow = document.querySelector('.exmplYellow');
-const blue = document.querySelector('.exmplBlue');
+const exmplYellow = document.querySelector('.exmplYellow');
+const exmplBlue = document.querySelector('.exmplBlue');
 
-let currentColor;
-let case1 = document.querySelector('#canvas tr td');
+let exmplColor;
+let case1 = document.querySelector('#exmplCanvas tr td');
 
-yellow.addEventListener('click', function(){
-    currentColor = yellow.textContent;
-    console.log(yellow);
+exmplYellow.addEventListener('click', function(){
+    exmplColor = exmplYellow.textContent;
+    console.log(exmplYellow);
 });
-blue.addEventListener('click', function(){
-    currentColor = blue.textContent;
-    console.log(blue);
+exmplBlue.addEventListener('click', function(){
+    exmplColor = exmplBlue.textContent;
+    console.log(exmplBlue);
 });
 
 case1.addEventListener('click', function(){
-    case1.classList.toggle(currentColor);
+    case1.classList.toggle(exmplColor);
     console.log(case1);
 })
 
@@ -24,3 +24,29 @@ case1.addEventListener('click', function(){
 
 ///////   EXERCICE   ///////
 
+const red = document.querySelector('.R');
+const yellow = document.querySelector('.J');
+const blue = document.querySelector('.B');
+const black = document.querySelector('.N');
+
+let currentColor;
+red.addEventListener('click', function(){
+    currentColor = red.textContent;
+});
+yellow.addEventListener('click', function(){
+    currentColor = yellow.textContent;
+});
+blue.addEventListener('click', function(){
+    currentColor = blue.textContent;
+});
+black.addEventListener('click', function(){
+    currentColor = black.textContent;
+});
+
+let cells = document.querySelectorAll('#canvas tr td');
+console.dir(cells);
+cells.forEach(cell => {
+    cell.addEventListener('click', function(){
+        cell.classList.toggle(currentColor);
+    });
+});
