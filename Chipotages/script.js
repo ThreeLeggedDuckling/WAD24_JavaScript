127e0ff315fb838bd40a377beb2949d1583bad5e
Melusine Chrs <melusine.chrs@gmail.com>
2024-06-11 13:35:25 +0200

    all good except tot

--- Chipotages/script.js
+++ Chipotages/script.js
@@ -27,7 +27,10 @@ margarita.addEventListener('click', function () {
 
 const productsTable = document.querySelector('#catalog tbody');
 const summary = document.querySelector('#bill tbody');
-const total = document.querySelector('#bill tfoot td:last-of-type');
+const total = document.querySelector('#bill tfoot tr:last-of-type td');
+
+let basket = 0
+total.textContent = basket.toFixed(2);
 
 const productsCol = productsTable.querySelectorAll('tr td:first-child');
 const addCol = productsTable.querySelectorAll('tr td:nth-last-child(2)');
@@ -38,13 +41,11 @@ let qntt = {};
 productsCol.forEach(product => {
     qntt[`${product.textContent}`] = 0;
 });
-console.log(qntt);
 
 
 addCol.forEach(cell => {
     cell.addEventListener('click', () => addTo(cell));
 });
-
 removeCol.forEach(cell => {
     cell.addEventListener('click', () => removeFrom(cell));
 })
@@ -52,35 +53,40 @@ removeCol.forEach(cell => {
 
 function addTo(cell) {
     prdName = cell.parentElement.children[0].textContent;
+    prdPrice = parseFloat(cell.parentElement.children[1].textContent);
     qntt[`${prdName}`]++;
-    prdTotPrc = parseFloat(cell.parentElement.children[1].textContent) * qntt[`${prdName}`];
+    prdTotPrc = prdPrice * qntt[`${prdName}`];
     prdTotPrc = prdTotPrc.toFixed(2);
 
+    basket += prdPrice;
+
     if (summary.querySelectorAll(`.${prdName}`).length > 0) {
-        summary.querySelector(`.${prdName}`).innerHTML = `<td>${prdName}</td><td>${qntt[`${prdName}`]}</td><td>${prdTotPrc}</td>`;
+        summary.querySelector(`.${prdName}`).innerHTML = `<td>${prdName}</td><td>${qntt[`${prdName}`]}</td><td class="priceT">${prdTotPrc}</td>`;
     }
     else {
-        summary.innerHTML += `<tr class='${prdName}'><td>${prdName}</td><td>${qntt[`${prdName}`]}</td><td>${prdTotPrc}</td></tr>`;
+        summary.innerHTML += `<tr class='${prdName}'><td>${prdName}</td><td>${qntt[`${prdName}`]}</td><td class="priceT">${prdTotPrc}</td></tr>`;
     };
 }
 
 function removeFrom(cell) {
     prdName = cell.parentElement.children[0].textContent;
-    if (qntt[`${prdName}`] > 0){
+    prdPrice = parseFloat(cell.parentElement.children[1].textContent);
+    prdLine = summary.querySelector(`.${prdName}`);
+    if (qntt[`${prdName}`] > 0) {
         qntt[`${prdName}`]--;
     }
-    prdTotPrc = parseFloat(cell.parentElement.children[1].textContent) * qntt[`${prdName}`];
+    prdTotPrc = prdPrice * qntt[`${prdName}`];
     prdTotPrc = prdTotPrc.toFixed(2);
 
-    if ((summary.querySelectorAll(`.${prdName}`).length > 0) && (qntt[`${prdName}`] > 0)) {
-        summary.querySelector(`.${prdName}`).innerHTML = `<td>${prdName}</td><td>${qntt[`${prdName}`]}</td><td>${prdTotPrc}</td>`;
+    if (qntt >= 0) {
+        basket -= prdPrice;
+        }
+    console.log(basket);
+
+    if (qntt[`${prdName}`] > 0) {
+        prdLine.innerHTML = `<td>${prdName}</td><td>${qntt[`${prdName}`]}</td><td class="priceT">${prdTotPrc}</td>`;
     }
     else {
-        summary.innerHTML -= `<tr class='${prdName}'><td>${prdName}</td><td>${qntt[`${prdName}`]}</td><td>${prdTotPrc}</td></tr>`;
-        summary.textContent = "";
+        prdLine.remove();
     };
 };
-
-
-// addA.addEventListener('click', () => addOne(addA));
-// remA.addEventListener('click', () => removeOne(remA));
